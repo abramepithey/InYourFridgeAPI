733dbf35fa7bcb7103d21207ae8bbc081dccd9e3
abramepithey <abramepithey@gmail.com>
2023-11-07 19:46:33 -0700

    Add data annotations to Ingredient model

--- InYourFridgeAPI/Models/Ingredient.cs
+++ InYourFridgeAPI/Models/Ingredient.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace InYourFridgeAPI.Models;
 
 public class Ingredient
 {
+    [Key]
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }
-    public DateTime PurchaseDate { get; set; }
-    public double ExpirationLength { get; set; }
+    public DateTime PurchaseDate { get; set; } = DateTime.Now;
+    public double ExpirationLength { get; set; } = (60 * 60 * 24 * 7);
 }
